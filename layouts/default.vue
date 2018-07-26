7d42b5de55305aa61a79754a76a4b87cf61b83b6
crea00 <654321su@gmail.com>
2018-07-26 21:49:30 +0900

    #020 Working with "Normal" Components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <Header />
     <nuxt/>
   </div>
 </template>
+
+<script>
+// @ : Behind the scenes by webpack which builds the project so this will be replaced with a root folder
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
 
 <style>
 </style>
